8f047564d446b9d74a646c3c7177455e2c256bc5
Sarah Khan <sarkhan@bu.edu>
2024-03-19 20:06:43 -0400

    made movie data class

--- app/src/main/java/com/example/duneapp/Movie.kt
+++ app/src/main/java/com/example/duneapp/Movie.kt
@@ -1,4 +1,10 @@
 package com.example.duneapp
 
-class Movie {
-}
+import android.widget.ImageView
+import java.io.Serializable
+
+data class News(
+    val title: String,
+    val year: Int,
+    val poster: ImageView
+)
