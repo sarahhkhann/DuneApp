cf7a292d19f2079d1db86892c1f92aec1ac028b3
Sarah Khan <sarkhan@bu.edu>
2024-03-19 20:46:21 -0400

    finalized ui

--- app/src/main/java/com/example/duneapp/MainActivity.kt
+++ app/src/main/java/com/example/duneapp/MainActivity.kt
@@ -1,13 +1,30 @@
 package com.example.duneapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val searchbutton = findViewById<Button>(R.id.searchbutton)
+        searchbutton.setOnClickListener {
+            replaceFragment()
+        }
+    }
+
+    private fun replaceFragment() {
+        val newFragment = ResultsFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, newFragment)
+        transaction.commit()
 
     }
+
+
 }
--- app/src/main/java/com/example/duneapp/apiservice.kt
+++ app/src/main/java/com/example/duneapp/apiservice.kt
@@ -2,6 +2,7 @@ package com.example.duneapp
 
 
 
+import android.graphics.Movie
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
